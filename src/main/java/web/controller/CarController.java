042903c57c876087fb755596baa3fa09f6df1fdd
Artur Makaryuk <art.makaryuk@gmail.com>
2023-04-22 13:40:55 +0700

    home

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,14 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.dao.CarDao;
-import web.model.Car;
 import web.service.CarService;
-import java.util.List;
 
 @Controller
 public class CarController {
-    private CarDao carDao;
-    private CarService carService;
+    private final CarDao carDao;
+    private final CarService carService;
 
     public CarController(CarDao carDao, CarService carService) {
         this.carDao = carDao;
@@ -22,14 +20,7 @@ public class CarController {
     @GetMapping(value = "/cars")
     public String carsPage(Model model,
                            @RequestParam(value = "count", required = false) Integer count) {
-        List<Car> cars = carDao.getAllCars();
-        List<Car> cars1;
-        if (null == count) {
-            cars1 = cars;
-        } else {
-            cars1 = carService.getCarsByCount(cars, count);
-        }
-        model.addAttribute("cars", cars1);
+        model.addAttribute("cars", carService.getCarsByCount(carDao.getAllCars(), count));
         return "cars";
     }
 
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,27 +1,28 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-
-@Component
+@Service
 public class CarServiceImpl implements CarService {
     @Override
-    public List<Car> getCarsByCount(List<Car> cars, int count) {
-        List<Car> cars1 = new ArrayList<>();
-        if (count == 0) {
-            return Collections.emptyList();
-        }
-        if (count < 0 | count > cars.size()) {
-            return cars;
-        } else {
-            for (int i = 0; i < count; i++)
-                cars1.add(cars.get(i));
-        }
-        return cars1;
+    public List<Car> getCarsByCount(List<Car> cars, Integer count) {
+        if (null != count) {
+            List<Car> cars1 = new ArrayList<>();
+            if (count == 0) {
+                return Collections.emptyList();
+            }
+            if (count < 0 | count > cars.size()) {
+                return cars;
+            } else {
+                for (int i = 0; i < count; i++)
+                    cars1.add(cars.get(i));
+            }
+            return cars1;
+        } else return cars;
     }
 }
 
